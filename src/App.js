9e69e5bdd7dca1825a4033a823810606821708cb
Saniya Syeda <ssyeda@wpi.edu>
2023-09-25 21:22:32 -0400

    fixed mobile version

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@ import { ToggleButtonGroup } from 'react-bootstrap';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import ToggleButton from 'react-bootstrap/ToggleButton';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Stack from 'react-bootstrap/Stack';
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
+import Spinner from 'react-bootstrap/Spinner';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   const [response, setResponse] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [value, setValue] = useState("scene")
   const [inputList, setInputList] = useState([]);
   const [catCount, setCatCount] = useState(1.0);
+
+  const radios = [
+    { name: 'scene', value: 'scene' },
+    { name: 'character', value: 'character' },
+    { name: 'object', value: 'object' },
+    { name: 'creature', value: 'creature' },
+    { name: 'situation', value: 'situation' }
+  ];
 
   const requestOptions = {
     method: 'POST',
@@ -24,25 +37,23 @@ function App() {
       messages: [{ role: "user", content: "Generate a random description of a " + value + " in 10 words as a drawing prompt" }]
     })
 
-};
-  //const apiKey = process.env.REACT_APP_API_KEY;
+  };
 
   const handleClick = () => {
+
+    setLoading(true)
     fetch('https://ideartist-server.netlify.app/.netlify/functions/server/data', requestOptions)
       .then((response) => response.json())
       .then((responseData) => {
         // console.log(responseData.answer.choices[0].message.content)
-        setResponse(responseData.answer.choices[0].message.content); 
+        setLoading(false);
+        setResponse(responseData.answer.choices[0].message.content);
       })
       .catch((error) => {
         console.error(error);
         setLoading(false);
       });
   };
-
-  const handleChange = (val) => {
-    setValue(val);
-  }
 
   const addBtnClick = event => {
     setCatCount(catCount + 1)
@@ -76,7 +87,7 @@ function App() {
       background-color: teal;
       color: white;
       border-radius: 1px;
-      margin: 10px;
+      margin: 7px;
     }
     .btn-outline-secondary {
       border-color: white;
@@ -85,62 +96,68 @@ function App() {
     `}</style>
 
 
-      <header className="fullBg App-header">
+      <header class="fullBg App-header">
 
-        <Stack className="justify-content-md-center align-items-center">
+        <Stack className="justify-content-center align-items-center">
           <h1 class="title display-5">Welcome to Ide<span>artist</span></h1>
           <p class="lead">Select a category to get started, or add your own!</p>
-          <ToggleButtonGroup type="radio" name="options" defaultValue={value} onChange={handleChange} >
-            <ToggleButton variant="flat" id="1" value="scene">scene</ToggleButton>
-            <ToggleButton variant="flat" id="2" value="character">character</ToggleButton>
-            <ToggleButton variant="flat" id="3" value="object">object</ToggleButton>
-            <ToggleButton variant="flat" id="4" value="creature">creature</ToggleButton>
-            <ToggleButton variant="flat" id="5" value="situation">situation</ToggleButton>
-            <ToggleButton onClick={addBtnClick} variant="flat" value="add">+</ToggleButton>
-          </ToggleButtonGroup>
-          <Stack direction="horizontal" gap={2} className="mx-auto">
-            {inputList.map((item) => (
 
-              <InputGroup className="mb-3" key={item.id}>
-                <Form.Control
-                  placeholder="Add Category"
-                  aria-label="Add Category"
+          <div>
+            <ButtonGroup className="m-1 choices">
+              {radios.map((radio, idx) => (
+                <ToggleButton
+                  key={idx}
+                  id={`radio-${idx}`}
+                  type="radio"
+                  variant="flat"
+                  name="radio"
+                  value={radio.value}
+                  checked={value === radio.value}
                   onChange={e => { setValue(e.target.value) }}
-                />
-                <Button onClick={() => removeCat(item.id)} variant="outline-secondary">
-                  x
-                </Button>
-              </InputGroup>
-            ))}
-
+                >
+                  {radio.name}
+                </ToggleButton>
+              ))}
+              <ToggleButton onClick={addBtnClick} variant="flat" value="add">+</ToggleButton>
+            </ButtonGroup>
+          </div>
+          <div className="addChoice">
+          <Stack direction="horizontal" gap={2} className="choices">
+          {inputList.map((item) => (
+            <InputGroup key={item.id}>
+              <Form.Control
+                placeholder="Add Category"
+                aria-label="Add Category"
+                onChange={e => { setValue(e.target.value) }}
+              />
+              <Button onClick={() => removeCat(item.id)} variant="outline-secondary">
+                x
+              </Button>
+            </InputGroup>
+          ))}
           </Stack>
-          {/* <div class="mt-2 mb-2">
-            <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
-              <ToggleButton variant="flat" id="tbg-radio-1" value={1}>category 1</ToggleButton>
-              <ToggleButton variant="flat" id="tbg-radio-2" value={2}>category 2</ToggleButton>
-              <ToggleButton variant="flat" id="tbg-radio-3" value={3}>category3</ToggleButton>
-            </ToggleButtonGroup>
-          </div> */}
+          </div>
 
           <div className="d-grid p-4 mt-1">
             <Button onClick={handleClick} size="lg" variant="dark">generate prompt!</Button>
           </div>
 
-          <div>
 
-            {response ? (
-              <p>{response}</p> // Conditionally render only when data is available
+            {loading ? (
+              <Spinner animation="border" />
             ) : (
-              <p></p> // Display a loading message while waiting for data
+              <div className="answerTxt">
+              <p class="lead"><em>{response}</em></p>
+              </div>
             )}
-            
-          </div>
+
+
 
         </Stack>
 
       </header>
 
-    </div>
+    </div >
   );
 }
 
